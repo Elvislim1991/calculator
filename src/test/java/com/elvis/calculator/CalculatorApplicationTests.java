47a4cd9ace6fdd6bdf1994c6ca40a1b0d81b38db
Elvis Lim <elvis.lim@webqlo.com>
2022-05-16 17:56:12 +0800

    commit calculator initial files

---
+++ src/test/java/com/elvis/calculator/CalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elvis.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
